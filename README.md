e78bb0993055c140df3c50f0f3338c806baed24b
Ryan Amorim <159044164+moon2501ry@users.noreply.github.com>
2025-03-01 14:20:08 -0300

    Update for configs

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Tired of taking too long and **_losing_** track of the _material_ on the _board_
 With **_FastText_** you **can** train your body to **read, copy and focus** **_better_** and **_faster_**.
 ### **!!DOWNLOAD NOW!!**
 [Download **here** (view releases)](https://github.com/moon2501ry/fast-text/releases)
+### HOW TO USE
+To change the text to be displayed, place it in "text.txt". To change the settings, simply modify them in "config.txt". The existing settings are: WPS (Words per second), COLOR (Text color), LANG (Language).
 ### VIEW MY PROFILE AND OTHERS PROJECTS
 [![GitHub Profile](https://img.shields.io/badge/GitHub-My_Profile-blue?style=for-the-badge&logo=github)](https://github.com/moon2501ry)
 [![Itch.io Profile](https://img.shields.io/badge/Itch.io-My_Profile-fa5c5c?style=for-the-badge&logo=itch.io&logoColor=white)](https://moon2501.itch.io/)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import ast
 
 pygame.init();
 display = pygame.display.set_mode((1280, 720));
+icon = pygame.image.load("data/icon.png");
+pygame.display.set_icon(icon);
 pygame.display.set_caption("Fast Text");
 clock = pygame.time.Clock();
 dt = 0;
@@ -10,26 +12,38 @@ tte = None;
 run = False;
 
 with open("config.txt", "r") as config:
-    wps = ast.literal_eval(config.read());
-with open("text.txt", "r") as file:
+    for c in config.readlines():
+        match c.split("=")[0]:
+            case "WPS":
+                wps = ast.literal_eval(c.split("=")[1].split(",")[0]);
+            case "COLOR":
+                color_text = c.split("=")[1].split(",")[0];
+            case "LANG":
+                lang = c.split("=")[1].split(",")[0];
+with open("text.txt", "r", encoding="utf-8") as file:
     w = file.read().split();
 index_w = 0;
 len_w = len(w);
 
 font = pygame.font.Font(None, 74);
-text = font.render("Welcome to the FastText. Press SPACE for begin", True, "red");
+match lang:
+    case "en":
+        text = font.render("Welcome to the FastText. Press SPACE for begin", True, "red");
+    case "pt":
+        text = font.render("Bem Vindo ao FastText. Pressione ESPAÇO", True, "red");
+    case _:
+        text = font.render("Welcome to the FastText. Press SPACE for begin", True, "red");
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit();
+        if event.type == pygame.QUIT: exit();
         if (event.type == pygame.KEYUP) and (event.key == pygame.K_SPACE):
-            if run != True: run = True;
+            if (run != True) and (tte is None): run = True;
 
     
     if run == True:
         if pygame.time.get_ticks() % (1000 // wps) < dt:
-            text = font.render(w[index_w], True, "blue");
+            text = font.render(w[index_w], True, color_text);
             index_w += 1;
             if index_w >= len_w:
                 index_w = 0;
@@ -37,8 +51,12 @@ while True:
                 run = False;
     elif (pygame.time.get_ticks() % (1000 // 1) < dt) and (tte is not None):
         tte -= 1;
-        text = text = font.render(f"Bye, exit the aplication in {tte}", True, "red");
-        if tte <= 0: quit();
+        match lang:
+            case "en":
+                text = font.render(f"Bye, exit the aplication in {tte}", True, "red");
+            case "pt":
+                text = font.render(f"Tchau, a aplicação fechará em {tte}", True, "red");
+        if tte <= 0: exit();
     
     display.fill("black");
 
