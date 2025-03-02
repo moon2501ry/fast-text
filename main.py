3d2ada7baf09e2a0a58338df305a1b5d9e112e16
Ryan Amorim <159044164+moon2501ry@users.noreply.github.com>
2025-03-02 16:55:22 -0300

    Atualização da pausa e alguns arquivos

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pygame
 import ast
 
+def pause(run):
+    match run:
+        case True:
+            run = False;
+            if edition == "jo2x":
+                pygame.display.set_caption("FastText - Stopped Time");
+                pygame.mixer.music.load("data/zawarudo.mp3");
+                pygame.mixer.music.play();
+        case False:
+            run = True;
+            pygame.mixer.music.unload();
+    return run;
+
 pygame.init();
 pygame.mixer.init();
 display = pygame.display.set_mode((1280, 720));
-icon = pygame.image.load("assets/icon.png");
+icon = pygame.image.load("data/icon.png");
 pygame.display.set_icon(icon);
 pygame.display.set_caption("FastText");
 clock = pygame.time.Clock();
@@ -23,6 +36,8 @@ with open("config.txt", "r") as config:
                 color_bg = c.split("=")[1].split(",")[0];
             case "LANG":
                 lang = c.split("=")[1].split(",")[0];
+            case "EDITION":
+                edition = c.split("=")[1].split(",")[0];
 with open("text.txt", "r", encoding="utf-8") as file:
     w = file.read().split();
 index_w = 0;
@@ -36,32 +51,20 @@ match lang:
     case "pt":
         text = font.render("Bem Vindo ao FastText", True, "red");
         button_text = font.render("COMEÇAR/PAUSAR", True, color_bg);
-button_spr = pygame.image.load("assets/button.png");
+button_spr = pygame.image.load("data/button.png");
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: exit();
         if (event.type == pygame.KEYUP) and (event.key == pygame.K_SPACE):
-            if (run == False) and (tte is None):
-                run = True;
-                pygame.mixer.music.unload();
-            elif (tte is None):
-                run = False;
-                pygame.display.set_caption("FastText - Stopped Time");
-                pygame.mixer.music.load("assets/zawarudo.mp3");
-                pygame.mixer.music.play();
+            if tte is None:
+                run = pause(run);
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_pos = event.pos;
             button_rect = button_spr.get_rect(center=(display.get_width()/2, display.get_height()/2 + display.get_height()/4));
             if button_rect.collidepoint(mouse_pos):
-                if (run == False) and (tte is None):
-                    run = True;
-                    pygame.mixer.music.unload();
-                elif (tte is None):
-                    run = False;
-                    pygame.display.set_caption("FastText - Stopped Time");
-                    pygame.mixer.music.load("assets/zawarudo.mp3");
-                    pygame.mixer.music.play();
+                if tte is None:
+                    run = pause(run);
     
     if run == True:
         if pygame.display.get_caption() != ('FastText', 'FastText'):
@@ -71,7 +74,10 @@ while True:
             index_w += 1;
             if index_w >= len_w:
                 index_w = 0;
-                tte = 5;
+                tte = 8;
+                if edition == "jo2x":
+                    pygame.mixer.music.load("data/roundabout.mp3");
+                    pygame.mixer.music.play();
                 run = False;
     elif (pygame.time.get_ticks() % (1000 // 1) < dt) and (tte is not None):
         tte -= 1;
